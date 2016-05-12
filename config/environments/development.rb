ec04d3e6b31afc7ad923a5a933b2039edf1da199
jtlindsey <reachjtl@gmail.com>
2016-05-12 10:18:50 -0400

    updated development.rb config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,13 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Email Message
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['EMAIL_ADDRESS'],
+    port:                 ENV[EMAIL_PORT],
+    domain:               ENV['EMAIL_DOMAIN'],
+    user_name:            ENV['EMAIL_USERNAME'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       ENV['EMAIL_AUTHENTICATION'],
+    enable_starttls_auto: true  }
 end

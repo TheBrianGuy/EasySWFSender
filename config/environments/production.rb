6b916a3ce0927d60b81989985010da26a329de55
jtlindsey <reachjtl@gmail.com>
2016-05-12 12:18:42 -0400

    updated config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,12 @@ Rails.application.configure do
   # Mail info for production
   config.action_mailer.default_url_options = { host: ENV['EMAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { port: 80 }
+  config.action_mailer.smtp_settings = {
+    address:              ENV['EMAIL_ADDRESS'],
+    port:                 587,
+    domain:               ENV['EMAIL_DOMAIN'],
+    user_name:            ENV['EMAIL_USERNAME'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       ENV['EMAIL_AUTHENTICATION'],
+    enable_starttls_auto: true  }
 end

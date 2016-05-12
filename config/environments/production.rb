43ba3f47192ec557f609b54c2eae875ff745e025
jtlindsey <reachjtl@gmail.com>
2016-05-12 12:40:21 -0400

    updated config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV['EMAIL_ADDRESS'],
-    port:                 465,
+    port:                 587,
     domain:               ENV['EMAIL_DOMAIN'],
     user_name:            ENV['EMAIL_USERNAME'],
     password:             ENV['EMAIL_PASSWORD'],

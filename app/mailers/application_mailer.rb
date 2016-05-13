207a958e9491a04bc9b993ac6576a7992ffd0c76
jtlindsey <reachjtl@gmail.com>
2016-05-13 08:01:32 -0400

    corrected comma error in mail config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  # default from: "from@example.com",
-  default from: ENV['EMAIL_FROM'],
+  # default from: "from@example.com"
+  default from: ENV['EMAIL_DEFAULT_FROM']
   layout 'mailer'
 end

4e0e655f441cfab1dbc3d742ce43e439cfbb5994
jtlindsey <reachjtl@gmail.com>
2016-05-13 07:48:44 -0400

    updated env parameters email_default_from

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -2,6 +2,7 @@ class FormMailer < ApplicationMailer
   def new_email(formroute, amessage)
     @amessage = amessage
     mail(to: formroute.fwd_to_email, 
+        reply_to: amessage.msg_from_email,
         subject: amessage.msg_subject)
   end
 end
